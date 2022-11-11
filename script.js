5c4156a8a8d56159c1a233ed41347f3aa5ac1a25
gopaladhikari <gopuadks@gmail.com>
2022-11-11 23:00:08 +0545

    Added nav-link collapse menu above 991px screen

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Slicing sub heading section
 let thirdSecHeading = document.getElementsByClassName("third-sec-heading");
 
 if (innerWidth < 768) {
@@ -8,7 +9,16 @@ if (innerWidth < 768) {
       : thirdSecHeading[i].innerHTML;
   }
 }
-//
+
+//Input box section
+let input = document.querySelector(".input").value;
+let btn = document.querySelector(".clear");
+
+const clickHandler = () => {
+  document.querySelector(".input").value = null;
+};
+
+// toggling FAQ for mobile
 let toggleListHeader = document.querySelectorAll(".toggle-list-header");
 let toggleList = document.querySelectorAll(".toggle-list");
 let footerIcon = document.querySelectorAll(".mobile-footer-icon");
