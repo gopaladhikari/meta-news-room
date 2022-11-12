4215782960e4a5eca1fdcdf148550a4b6b21adc6
gopaladhikari <gopuadks@gmail.com>
2022-11-12 22:59:09 +0545

    Fixed navbar icon rotation

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 //Slicing sub heading section
 let thirdSecHeading = document.getElementsByClassName("third-sec-heading");
-
 if (innerWidth < 768) {
   for (let i = 0; i < thirdSecHeading.length; i++) {
     thirdSecHeading[i].innerHTML.length > 60
@@ -18,6 +17,21 @@ const clickHandler = () => {
   document.querySelector(".input").value = null;
 };
 
+// Toggling nav-link icon direction
+let a = document.querySelectorAll(".a");
+let icon = document.querySelectorAll(".icon");
+
+for (let i = 0; i < a.length; i++) {
+  a[i].addEventListener("click", () => {
+    icon[i].classList.toggle("rotate-nav-icon");
+    for (let j = 0; j < a.length; j++) {
+      if (i != j && icon[j].classList.contains("rotate-nav-icon")) {
+        icon[j].classList.remove("rotate-nav-icon");
+      }
+    }
+  });
+}
+
 // toggling FAQ for mobile
 let toggleListHeader = document.querySelectorAll(".toggle-list-header");
 let toggleList = document.querySelectorAll(".toggle-list");
@@ -25,7 +39,7 @@ let footerIcon = document.querySelectorAll(".mobile-footer-icon");
 
 for (let i = 0; i < toggleListHeader.length; i++) {
   toggleListHeader[i].addEventListener("click", () => {
+    footerIcon[i].classList.toggle("rotate-nav-icon");
     toggleList[i].classList.toggle("show-list");
-    footerIcon[i].classList.toggle("header-container");
   });
 }
